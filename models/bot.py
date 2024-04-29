c87f1c593b785718aaa7b1f95bce19d2385944f3
AslDev006 <asldev006@gmail.com>
2024-04-29 14:56:04 +0500

    BOT

--- models/bot.py
+++ models/bot.py
@@ -8,8 +8,9 @@ bot = TeleBot(token=env("TOKEN"))
 
 
 
-
-def get_post(data):
-    bot.send_message(chat_id=5322589899, text=f"Sizga yangi murojaat mavjud !\n\n\n"
+ids = [5823007647, 5322589899]
+for i in ids:
+    def get_post(data):
+        bot.send_message(chat_id=i, text=f"Sizga yangi murojaat mavjud !\n\n\n"
                                               f"Ism: {data['name']}\n"
                                               f"Telefon Raqam: {data['phone_number']}")
